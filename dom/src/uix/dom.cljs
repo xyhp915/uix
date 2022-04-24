dcf49ec9ac41fe24ca5e745c4ea657a92129948b
roman01la <roman01la@romanliutikov.com>
2022-04-25 01:04:57 +0300

    add rd18 api

--- dom/src/uix/dom.cljs
+++ dom/src/uix/dom.cljs
@@ -4,55 +4,58 @@
 
 ;; react-dom top-level API
 
-(defn render
-  "Renders element into DOM node. The first argument is Hiccup or React element."
-  [element node]
-  (rdom/render element node))
+(defn create-root
+  "Create a React root for the supplied container and return the root.
 
-(defn create-root [node]
-  (rdom/unstable_createRoot node))
+  See: https://reactjs.org/docs/react-dom-client.html#createroot"
+  ([node]
+   (rdom/createRoot node))
+  ([node {:keys [on-recoverable-error identifier-prefix] :as options}]
+   (rdom/createRoot node #js {:onRecoverableError on-recoverable-error
+                              :identifierPrefix identifier-prefix})))
 
-(defn render-root [element root]
+(defn hydrate-root
+  "Same as `create-root`, but is used to hydrate a container whose HTML contents were rendered by ReactDOMServer.
+
+  See: https://reactjs.org/docs/react-dom-client.html#hydrateroot"
+  ([container element]
+   (rdom/hydrateRoot container element))
+  ([container element {:keys [on-recoverable-error identifier-prefix] :as options}]
+   (rdom/hydrateRoot container element #js {:onRecoverableError on-recoverable-error
+                                            :identifierPrefix identifier-prefix})))
+
+(defn render-root
+  "Renders React root into the DOM node."
+  [element root]
   (.render root element))
 
-(defn hydrate
-  "Hydrates server rendered document at `node` with `element`."
-  [element node]
-  (rdom/hydrate element node))
+(defn unmount-root
+  "Remove a mounted React root from the DOM and clean up its event handlers and state."
+  [root]
+  (.unmount root))
 
-(defn flush-sync! [cb]
-  (rdom/flushSync cb))
+(defn flush-sync
+  "Force React to flush any updates inside the provided callback synchronously.
+  This ensures that the DOM is updated immediately.
 
-(defn flush-controlled! [cb]
-  (rdom/unstable_flushControlled cb))
+  See: https://reactjs.org/docs/react-dom.html#flushsync"
+  [callback]
+  (rdom/flushSync callback))
 
-(defn unmount-at-node
-  "Unmounts React component rendered into DOM node"
-  [node]
-  (rdom/unmountComponentAtNode node))
+(defn batched-updates [f]
+  (rdom/unstable_batchedUpdates f))
 
 (defn find-dom-node
-  "Returns top-level DOM node associated with component"
+  "If this component has been mounted into the DOM, this returns the corresponding native browser DOM element.
+
+  See: https://reactjs.org/docs/react-dom.html#finddomnode"
   [component]
   (rdom/findDOMNode component))
 
 (defn create-portal
-  "Renders Hiccup element into DOM node"
+  "Creates a portal. Portals provide a way to render children into a DOM node
+  that exists outside the hierarchy of the DOM component.
+
+  See: https://reactjs.org/docs/react-dom.html#createportal"
   [child node]
   (rdom/createPortal child node))
-
-;; react-dom/server top-level API
-
-(defn render-to-string [element]
-  "Renders to HTML string to be used with React"
-  (.renderToString js/ReactDOMServer element))
-
-(defn render-to-static-markup [element]
-  "Renders to HTML string"
-  (.renderToStaticMarkup js/ReactDOMServer element))
-
-(defn render-to-stream [element]
-  (.renderToNodeStream js/ReactDOMServer element))
-
-(defn render-to-static-stream [element]
-  (.renderToStaticNodeStream js/ReactDOMServer element))
