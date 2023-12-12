6de3af7d58cfd25b0d816a7780bbe3c90e164016
Roman Liutikov <roman01la@romanliutikov.com>
2023-12-12 18:34:03 +0200

    improve reporting

--- dom/src/uix/dom.clj
+++
@@ -1,9 +0,0 @@
-(ns uix.dom
-  (:require [uix.linter]
-            [uix.dom.linter]
-            [uix.lib]))
-
-(defmethod uix.linter/lint-element :dom/invalid-attribute [type form env]
-  (let [[tag attrs] (uix.lib/normalize-element env (rest form))]
-    (when (and (keyword? tag) (map? attrs))
-      (uix.dom.linter/validate-attrs tag attrs {:env env}))))
